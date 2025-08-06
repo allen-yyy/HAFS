f4ee5851782b9ddb4dd784a7ac19261bc2de1e4a
Allen He <59130189+allen-yyy@users.noreply.github.com>
2025-08-06 22:08:19 +0800

    HAFS For Windows Version 0.9

---
+++ Makefile
@@ -0,0 +1,2 @@
+all:
+	g++ -static HAFS.c Windows.cpp -o hafs_for_windows.exe
--- test.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-int main()
-{
-    long long a=1145141919810;
-    printf("%lld",a);   
-    return 0;
-}
